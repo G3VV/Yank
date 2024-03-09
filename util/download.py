4a0266a78cbee21bcab8d0bf6389e71093b74c32
G3V <46306494+G3VV@users.noreply.github.com>
2024-03-09 23:53:05 +0000

    Removed debugging and cleaned up code

--- util/download.py
+++ util/download.py
@@ -14,6 +14,7 @@ import sys
 import json
 import shutil
 
+# what a mess of code
 
 load_dotenv()
 download_dir = "./music/"
@@ -24,31 +25,27 @@ try:
     deezer = Deezer(arl=arl)
     print("Logged into Deezer")
 except:
-    print("Error logging into Deezer")
+    print("Error logging into Deezer, check ARL is correct inside .env")
 
 
 async def start(id):
     isrc = id
     try:
-
         try:
             track = await spotify_isrc(isrc)
         except Exception as e:
-            print("Spotify token expired or couldn't find isrc")
+            print("Spotify token expired or couldn't find ISRC")
             print(" ")
             print(e)
             return "none"
-
         if 'isrc' in track['external_ids']:
             isrc = track['external_ids']['isrc']
         else:
             isrc = "ISRC not available"
             print("Song not found")
             return "none"
-
         j = await get_deezer_track(isrc)
         pathfile = Path(f"./music/{isrc}.mp3")
-
         if pathfile.is_file():
             print(f"[{isrc}] Already cached")
             return pathfile
@@ -57,12 +54,11 @@ async def start(id):
             try:
                 track_id = j["id"]
             except:
-                print("Couldn't find song on deezer")
+                print("Couldn't find song on Deezer")
                 return "none"
             loop = asyncio.get_event_loop()
             download_track(track_id, isrc)
             return pathfile
-
     except Exception as e:
         print(f"{e} at line {sys.exc_info()[-1].tb_lineno}")
         return "none"
@@ -70,18 +66,14 @@ async def start(id):
 async def start_playlist(id):
     folder_to_zip = f'./music/{id}/'
     output_zip_file = f'./zip/{id}'
+    isrc = id
     def zip_folder(folder_path, output_path):
         print(f"[playlist] Zipping folder {folder_path} to {output_path}")
-
         shutil.make_archive(output_path, 'zip', folder_path)
         print(f"[playlist] Finished zipping folder {folder_path} to {output_path}")
-
-
-    isrc = id
     try:
         if os.path.exists(folder_to_zip):
             return output_zip_file + ".zip"
-
         try:
             playlist_isrcs = await spotify_playlist(isrc)
         except Exception as e:
@@ -89,27 +81,17 @@ async def start_playlist(id):
             print(" ")
             print(e)
             return "none"
-
         deezer_ids = []
-
         for index in range(len(playlist_isrcs)):
             try:
                 j = await get_deezer_track(playlist_isrcs[index])
-                print(j["id"])
                 deezer_ids.append(f'{j["id"]}')
             except:
                 print("Couldn't find song on deezer")
                 continue
-
-        #return deezer_ids
-
-
-
         download_playlist(deezer_ids, id)
-
         zip_folder(folder_to_zip, output_zip_file)
         return output_zip_file + ".zip"
-
     except Exception as e:
         print(f"{e} at line {sys.exc_info()[-1].tb_lineno}")
         return "none"
@@ -117,18 +99,14 @@ async def start_playlist(id):
 
 
 def download_track(track_id, isrc):
-
     track = deezer.get_track(track_id)
-
     print(f"[{isrc}] Starting download")
     track["download"](download_dir, quality=track_formats.MP3_320, filename=isrc, with_lyrics=False, show_message=False)
     print(f"[{isrc}] Finished download")
 
 def download_playlist(id_list, playlist_id):
     print(f"[playlist] Starting download")
-
     download_dir = f"./music/{playlist_id}/"
     downloader = Downloader(deezer, id_list, download_dir, quality=track_formats.MP3_320, concurrent_downloads=25)
     downloader.start()
-
     print(f"[playlist] Finished download")
