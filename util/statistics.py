bcd11fbb96ca1e2b112de667c72f64cb56713edb
G3V <46306494+G3VV@users.noreply.github.com>
2024-07-15 05:56:57 +0100

    fix for statistics

--- util/statistics.py
+++ util/statistics.py
@@ -4,10 +4,12 @@ song_directory = "music"
 playlist_directory = "zip"
 
 async def totalCaches():
-    return len(os.listdir(song_directory)) + len(os.listdir(playlist_directory))
+    song_count = len(os.listdir(song_directory)) if os.path.exists(song_directory) else 0
+    playlist_count = len(os.listdir(playlist_directory)) if os.path.exists(playlist_directory) else 0
+    return song_count + playlist_count
 
 async def totalSongs():
-    return len(os.listdir(song_directory))
+    return len(os.listdir(song_directory)) if os.path.exists(song_directory) else 0
 
 async def totalPlaylists():
-    return len(os.listdir(playlist_directory))
+    return len(os.listdir(playlist_directory)) if os.path.exists(playlist_directory) else 0
